d2bc548ca9e18188cb6edcc3f4fc592769042d2c
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2024-01-14 17:48:24 -0300

    Paginação perfil por usuario

--- backend/src/main/java/br/com/gabrielferreira/contratos/api/mapper/PerfilMapper.java
+++ backend/src/main/java/br/com/gabrielferreira/contratos/api/mapper/PerfilMapper.java
@@ -2,7 +2,10 @@ package br.com.gabrielferreira.contratos.api.mapper;
 
 import br.com.gabrielferreira.contratos.api.model.PerfilModel;
 import br.com.gabrielferreira.contratos.api.model.input.PerfilInputModel;
+import br.com.gabrielferreira.contratos.api.model.params.PerfilParamsModel;
 import br.com.gabrielferreira.contratos.domain.model.Perfil;
+import br.com.gabrielferreira.contratos.domain.repository.filter.PerfilFilterModel;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -33,5 +36,17 @@ public class PerfilMapper {
         List<Perfil> novosPerfis = new ArrayList<>();
         perfis.forEach(perfil -> novosPerfis.add(toPerfil(perfil)));
         return novosPerfis;
+    }
+
+    public PerfilFilterModel toPerfilFilterModel(PerfilParamsModel perfilParamsModel){
+        return PerfilFilterModel.builder()
+                .id(perfilParamsModel.getId())
+                .descricao(perfilParamsModel.getDescricao())
+                .autoriedade(perfilParamsModel.getAutoriedade())
+                .build();
+    }
+
+    public Page<PerfilModel> toPerfisModels(Page<Perfil> perfis){
+        return perfis.map(this::toPerfilModel);
     }
 }
